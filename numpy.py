8275ad671cc9498170398be1ee54a776da621859
BobbyHarish <harishkarnan004@gmail.com>
2023-09-28 08:50:48 +0530

    Numpy using Python

---
+++ numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+h=[]
+for i in range (int(input("Enter the N value:"))):
+    details=[]
+    details.append(input("Enter the name:"))
+    h.append(details)
+
+m=np.array(((h)))
+print(m)
