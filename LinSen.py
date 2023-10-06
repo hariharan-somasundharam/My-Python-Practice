214bf78073bb120c9da3bddb4b041862114bc035
BobbyHarish <harishkarnan004@gmail.com>
2023-10-06 22:11:35 +0530

    Linear Search Program in Python

---
+++ LinSen.py
@@ -0,0 +1,20 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # Return the index where the target element is found
+    return -1  # Return -1 if the target element is not in the list
+
+# Example usage:
+my_list=[]
+n=int(input("Enter the Range:"))
+
+for i in range (n):
+    
+    my_list.append(int(input()))
+target_element = int(input("Enter the search element:"))
+
+result = linear_search(my_list, target_element)
+if result != -1:
+    print(f"Element {target_element} found at index {result}")
+else:
+    print(f"Element {target_element} not found in the list")
