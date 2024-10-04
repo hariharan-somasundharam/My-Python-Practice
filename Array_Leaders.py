22cfe736c4171baab8985f1473d4bd5fe58499e7
HariHaran <harishkarnan004@gmail.com>
2024-10-04 23:47:15 +0530

    Array Leaders in Python
    Signed-off-by: HariHaran <harishkarnan004@gmail.com>

---
+++ Array_Leaders.py
@@ -0,0 +1,21 @@
+def leaders(n, arr):
+        maxi=arr[-1]
+        leaders=[]
+        
+        for i in arr[::-1]:
+            maxi=max(maxi,i)
+            if i>=maxi:
+                leaders.append(i)
+                
+                
+        return leaders[::-1]
+
+n=int(input("Enter the Array Size:"))
+arr=[]
+print("Enter the Array Elements:")
+
+for i in range(n):
+     arr.append(int(input()))
+
+arr = leaders(n,arr)
+print(arr)
