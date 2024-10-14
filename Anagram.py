a999510caec2ee0b661f874ee8966ae30fa6cd8c
HariHaran <harishkarnan004@gmail.com>
2024-10-14 22:31:05 +0530

    Anagram Signed-off-by: HariHaran <harishkarnan004@gmail.com>

---
+++ Anagram.py
@@ -0,0 +1,12 @@
+def isAnagram(str1, str2):
+    if sorted(str1) == sorted(str2):
+        return True
+    else:
+        return False
+str1 = input("String 1 : ")
+str2 = input("String 2 : ")
+print("Is Anagram : ", isAnagram(str1,str2))
+
+# String 1 : HariHaran
+# String 2 : HaranHari
+# Is Anagram :  True
