cb84eecc37fe004a48e456983f20d0ae600c4453
HariHaran <142760572+hariharan-somasundharam@users.noreply.github.com>
2024-10-15 21:30:37 +0530

    Anagram

--- Anagram.py
+++ Anagram.py
@@ -7,6 +7,6 @@ str1 = input("String 1 : ")
 str2 = input("String 2 : ")
 print("Is Anagram : ", isAnagram(str1,str2))
 
-# String 1 : HariHaran
-# String 2 : HaranHari
+# String 1 : HariHaranS
+# String 2 : HaranHariS
 # Is Anagram :  True
